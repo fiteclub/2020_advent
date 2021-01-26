26e81078bddb5941ff580db720b1606563547112
fiteclub <mattf255@yahoo.com>
2021-01-26 13:31:38 -0500

    Refactor Day One, Part Two

--- day_01/report_repair_part_2.rb
+++ day_01/report_repair_part_2.rb
@@ -3,52 +3,24 @@ require 'pry'
 
 target_sum = 2020
 file = "input.txt"
-input_data = File.read("input.txt")
-integer_array = input_data.split.map { |string| string.to_i }
+integer_array = File.read("input.txt").split.map { |string| string.to_i }
 
-def find_sum_pair(input_array, target)
-  sum_pair = [nil, nil]
-  input_array.each do |entry|
-    puts "Searching #{entry_prime}"
-    difference = (target.to_i - entry.to_i)
-    # puts "Trying #{entry}, difference of #{difference}"
-    if input_array.include?(difference) && difference > 0
-      sum_pair[0] = entry
-      sum_pair[1] = difference
-      # puts "Match found! #{sum_pair[0]} and #{sum_pair[1]}"
-      puts "Answer: #{sum_pair[0]} + #{sum_pair[1]} = #{target}"
-      sum_pair
-    else
-      false
-    end
-    # p sum_pair
-  end
-    # if
-    #   puts "Answer: #{sum_pair[0]} + #{sum_pair[1]} = #{target}"
-    # sum_pair
-end
-
-def find_sum_triplet(input_array_prime, target_prime)
-  sum_triplet = [nil, nil]
-  input_array_prime.each do |entry_prime|
-    difference_prime = target_prime.to_i - entry_prime.to_i
-    pair = find_sum_pair(input_array_prime, difference_prime)
-    if pair != false && input_array_prime.include?(difference_prime) && difference_prime > 0
-      sum_triplet[0] = entry_prime
-      sum_triplet[1] = [pair[0], pair[1]]
-      puts "TRIPLET Answer: #{sum_triplet[0]} + #{sum_triplet[1][0]} + #{sum_triplet[1][1]} = #{target_prime}"
-      sum_triplet
-    else
-      false
+def find_sum_pairs(array, target_sum)
+  raise 'exception' if !target_sum.is_a? Integer || target_sum < 1
+  return_array = Array.new
+  sum_pair = Array.new
+  array.each do |entry|
+    difference = (target_sum.to_i - entry.to_i)
+    if difference.positive? && array.include?(difference)
+      sum_pair[0] = entry.to_i
+      sum_pair[1] = difference.to_i
+      puts "Match: #{sum_pair[0]} + #{sum_pair[1]} = #{target_sum}"
+      return_array << sum_pair
     end
   end
+  return_array.uniq!
 end
 
 binding.pry
-
-
-
-answer = find_sum_triplet(integer_array, target_sum)
-puts answer[0] * answer[1] * answer[2]
 
 puts "end"
