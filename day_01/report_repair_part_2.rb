9c4e45129fd7ea1ac11a17c42d6d1d1fbe1b745f
fiteclub <mattf255@yahoo.com>
2021-01-26 13:57:57 -0500

    Solve Day One, Part Two

--- day_01/report_repair_part_2.rb
+++ day_01/report_repair_part_2.rb
@@ -7,6 +7,7 @@ integer_array = File.read("input.txt").split.map { |string| string.to_i }
 
 def find_sum_pairs(array, target_sum)
   raise 'exception' if !target_sum.is_a? Integer || target_sum < 1
+
   return_array = Array.new
   sum_pair = Array.new
   array.each do |entry|
@@ -14,13 +15,30 @@ def find_sum_pairs(array, target_sum)
     if difference.positive? && array.include?(difference)
       sum_pair[0] = entry.to_i
       sum_pair[1] = difference.to_i
-      puts "Match: #{sum_pair[0]} + #{sum_pair[1]} = #{target_sum}"
+      puts " Match Pair: #{sum_pair[0]} + #{sum_pair[1]} = #{target_sum}"
       return_array << sum_pair
     end
   end
   return_array.uniq!
 end
 
-binding.pry
+def find_triplet(array, target_sum)
+  return_array = Array.new
+  sum_triplet = Array.new
+  array.each do |entry|
+    addend_pair = find_sum_pairs(array, (target_sum - entry))
+    if addend_pair != nil
+      sum_triplet[0] = addend_pair[0][0].to_i
+      sum_triplet[1] = addend_pair[0][1].to_i
+      sum_triplet[2] = entry.to_i
+      puts "Triplet Match : #{sum_triplet[0]} + #{sum_triplet[1]} + #{sum_triplet[2]} = #{target_sum}"
+      return_array << sum_triplet
+    end
+  end
+  return_array.uniq!
+end
 
-puts "end"
+answer_array = find_triplet(integer_array, target_sum)
+puts "---"
+puts 'Product of first triplet:'
+puts (answer_array[0][0] * answer_array[0][1] * answer_array[0][2])
