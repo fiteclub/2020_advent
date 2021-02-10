4b14f205832f06178b413f61fb1dd2a19839e5e5
fiteclub <mattf255@yahoo.com>
2021-02-10 17:22:01 -0500

    Create day 6 specs

--- spec/day_06_spec.rb
+++ spec/day_06_spec.rb
@@ -1,55 +1,42 @@
 require_relative '../day_06/customs_00'
 require_relative '../helpers'
 
+testdata = "abc\n\na\nb\nc\n\nab\nac\n\na\na\na\na\n\nb\n"
+testplane = CustomsPlane.new(testdata)
+testgroup = testplane.groups[1]
 
-# How are you going to do this? Part One:
-# 1) Separate input into group array with member hashes for answers
-#    example: group[2][3] = 'c'
-#
-# 2) Create a main array of empty letter hashes with a-z keys,
-#    and int values for the sum of each group
-#    example: group[1] = {a: 1, b: 1, c: 1}
-#
-# 3) Iterate over each member in each group, adding to the
-#    appropriate group letter key's sum
-#
-# 4) Iterate over each group, incrementing an a-z hash each time a
-#    letter is answered by anyone in the group, but only once for
-#    the group.
-#
-# 4) Profit
 
-# Plane class
-# CustomsGroup class
-# Member class
-# A Plane has many CustomsGroups
-# A CustomsGroup has many Members
-# A Member has one answers string
+RSpec.describe 'CustomsPlane class' do
+  it 'returns the original input string' do
+    expect(testplane.plane).to eq(testdata)
+  end
 
-testdata = get_data('day_06/test_input.txt')
-# Each line is one person within a group.
-# Group breaks where there are two newlines
+  it 'returns groups of passengers' do
+    expect(testplane.groups.count).to eq(5)
+    expect(testplane.groups[1].passengers).to eq(['a','b','c'])
+  end
 
-RSpec.describe 'Plane class' do
-  it 'sums the answers of all groups, returns a hash '
+  it 'returns letters and sum of yes answers' do
+    expect(testplane.totals).to eq({:a=>8, :b=>4, :c=>3})
+  end
+
+  it 'returns letters marked yes' do
+    expect(testplane.saidyes).to eq(11)
+  end
+
 end
 
 RSpec.describe 'CustomsGroup class' do
-  it 'converts raw input into member answers' do
-    # expect(testgroup.members)
+  it 'returns passenger answers' do
+    expect(testplane.groups[1].passengers).to eq(['a','b','c'])
+  end 
+
+  it 'sums the answers of group passengers, returns a hash' do
+    expect(testplane.groups[1].totals).to eq({:a=>1, :b=>1, :c=>1})
   end
 
-  it 'sums the answers of group members, returns a hash' do
-    # expect(testgroup.sum)
-  end
-
-  it 'returns a hash of letters answered at least once' do
-    # expect(testgroup.uniq)
-  end
-end
-
-RSpec describe 'Member class' do
-  it 'returns a hash of letters' do
-    # expect(testmember.answers).to eq({a})
+  it 'sums the total number of letters marked yes' do
+    expect(testplane.groups[2].yes).to eq(3)
+    expect(testplane.groups[3].yes).to eq(1)
   end
 end
