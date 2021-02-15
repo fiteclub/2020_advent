6989e198709043e8d09a8e79ad50a3ee87a1167d
fiteclub <mattf255@yahoo.com>
2021-02-15 18:13:25 -0500

    Refactor classes

--- day_07/bag_00.rb
+++ day_07/bag_00.rb
@@ -2,23 +2,38 @@
 require_relative '../helpers'
 require 'multiset'
 
+
 class Bag
   include Enumerable
-  attr_accessor :contents
+  attr_accessor :contents, :name
 
-  def initialize
-    @contents = {}
+  def initialize(name, contents)
+    @name = name
+    @contents = contents
   end
 
-  def contains(search_item)
-    search_term.class != 'Symbol' ? (search_item = search_item.to_sym) : nil
-    @contents[search_item] != nil
+  def has_inside(baglist, bag_name, searchlist)
+    searched = searchlist
+    searched << @name
+    puts "Searching #{@name} for #{bag_name}"
+    return self if bag_name == @name
+    puts "Searching contents of #{@name} for #{bag_name}"
+    contents.keys.each do |inside_bag|
+      puts "--- searching #{baglist[inside_bag].name} for #{bag_name}..."
+      found_bag = baglist[inside_bag].has_inside(baglist, bag_name, searched)
+      if !found_bag.nil?
+        searched << bag_name
+        puts searched.to_s
+        searched = []
+      end
+      return found_bag unless found_bag.nil?
+    end
+    nil
   end
 
   def each(&block)
     @contents.each(&block)
   end
-
 end
 
 class BagHandler
@@ -33,32 +48,26 @@ class BagHandler
 
   def build_list
     @input.each do |line|
-      bag_name = line[/^(.*?)(?= bag)/, 1].tr(' ', '_').to_sym
+      bag_name = line[/^(.*?)(?= bag)/, 1].tr(' ', '_').to_s
       bag_contents = line[/[1-9].*(?=\.)/]
-      new_bag= {}
+      new_bag_contents = {}
       if !bag_contents.nil?
         bag_contents.split(", ").each do |string|
-          sub_bag = string[/(?![0-9])[a-z].*(?= bag)/].tr(' ', '_').to_sym
+          interior_bag = string[/(?![0-9])[a-z].*(?= bag)/].tr(' ', '_').to_sym
           num_bags = string[/[0-9]*/].to_i
-          new_bag[sub_bag.to_sym] = num_bags
+          new_bag_contents[interior_bag.to_sym] = num_bags
         end
       end
-    @list[bag_name] = new_bag
+      @list[bag_name.to_sym] = Bag.new(bag_name, new_bag_contents)
     end
   end
 
-  def find(search_key, *primary_search)
-    @list.each do |bag|
-      if bag.to_a[1].empty?
-        next
-      elsif bag.to_a[1].has_key?search_key
-        puts bag
-      else
-        bag.to_a[1].keys.each do |sub_bag|
-          find(sub_bag)
-        end
-      end
+  def search(target_bag)
+    return_array = []
+    @list.each_with_object({}) do |object|
+      return_array << object[1].has_inside(@list, target_bag, [])
     end
+    return_array
   end
 
   def each(&block)
--- day_07/bag_01.rb
+++ day_07/bag_01.rb
@@ -2,7 +2,7 @@
 require_relative '../helpers'
 require_relative 'bag_00'
 
-# data = get_data("input.txt")
+# data = get_data("input.txt", type = 'array')
 data = get_data("test_input.txt", type = 'array')
 b = BagHandler.new(data)
 b.build_list
