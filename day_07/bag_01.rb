60e7e33eccaa5a55b14720ee79154076aa41647e
fiteclub <mattf255@yahoo.com>
2021-02-16 01:09:40 -0500

    Solve day seven, part one

--- day_07/bag_01.rb
+++ day_07/bag_01.rb
@@ -2,10 +2,8 @@
 require_relative '../helpers'
 require_relative 'bag_00'
 
-# data = get_data("input.txt", type = 'array')
-data = get_data("test_input.txt", type = 'array')
+looking_for = 'shiny_gold'
+data = get_data("input.txt", 'array')
 b = BagHandler.new(data)
 b.build_list
-
-binding.pry
-puts "#{colorize('end')}"
+puts colorarray([b.search(looking_for).length, 'bag colors can eventually contain at least one', looking_for.to_sym, 'bag']).join(' ')
