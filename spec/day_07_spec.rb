011319f281dd4394829bd1a59be2a996ceb0d032
fiteclub <mattf255@yahoo.com>
2021-02-16 01:07:59 -0500

    Refactor tests for new classes

--- spec/day_07_spec.rb
+++ spec/day_07_spec.rb
@@ -1,25 +1,62 @@
 require_relative '../day_07/bag_00'
 require_relative '../helpers'
 
-test_handler = BagHandler.new(get_data('day_07/test_input.txt'))
+test_handler = BagHandler.new(get_data('day_07/test_input.txt', 'array'))
+test_bag = test_handler.list.first[1]
 
-
-RSpec.describe 'BagHandler class' do
-  it 'extracts bag name from raw input' do
-    # expect(test_handler[0]).to eq()
+RSpec.describe 'Bag class' do
+  it 'returns name of bag' do
+    expect(test_bag.name).to eq("light_red")
   end
 
-  it 'extracts contained bags' do
-    # expect(test_handler[0]).to eq()
+  it 'returns contents of bag' do
+    expect(test_bag.contents).to eq({ :bright_white=>1, :muted_yellow=>2 })
+  end
+
+  it 'has enumerator methods' do
+    expect(test_bag.each).to be_an Enumerator
+  end
+
+  it 'returns an array if a given bag can be found inside' do
+    expect(test_bag.has_inside(test_handler.list, 'shiny_gold')).to eq(
+      ['light_red', 'bright_white', 'shiny_gold' ]
+    )
+  end
+
+  it 'returns nil if a given bag cannot be found inside' do
+    expect(test_bag.has_inside(test_handler.list, 'hideous_green')).to be nil
   end
 end
 
-RSpec.describe 'Bag class' do
-  it 'returns bag name' do
-    
+
+
+RSpec.describe 'BagHandler class' do
+  it 'takes in raw data array' do
+    expect(test_handler.input[0].slice(0,9)).to eq("light red")
   end
 
-  it 'returns contained bags' do
-
+  it 'takes in raw data and creates a hash of Bag objects' do
+    expect(test_handler.list.first[0]).to eq(test_bag.name.to_sym)
+    expect(test_handler.list.first[1].name).to eq(test_bag.name)
+    expect(test_handler.list.first[1].contents).to eq(test_bag.contents)
   end
+
+  it 'has enumerator methods' do
+    expect(test_handler.each).to be_an Enumerator
+  end
+
+  it 'searches for a given bag and returns array of possible bag container combinations' do
+    expect(test_handler.search('shiny_gold')).to eq([
+        [ 'light_red', 'bright_white', 'shiny_gold' ],
+        [ 'dark_orange', 'bright_white', 'shiny_gold' ],
+        [ 'bright_white', 'shiny_gold' ],
+        [ 'muted_yellow', 'shiny_gold' ]
+    ])
+  end
+
+  test_handler.list[:awful_green] = Bag.new('awful_green',{})
+  it 'searches for a given bag and returns nil if the bag is not found in any other bags' do
+    expect(test_handler.search('awful_green')).to be nil
+  end
+
 end
